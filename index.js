0c12241223dc3911081584e7495bd2b46ae41c1b
Georg Freitag <georg.freitag@gmail.com>
2017-11-15 18:24:53 +0100

    upgrade auf webpack

---
+++ index.js
@@ -0,0 +1,5 @@
+import moment from 'moment';
+console.log("Hello from JavaScript 2000!");
+console.log(moment().startOf('day').fromNow());
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
